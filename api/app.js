a14b0e1eaba45f16d621827d9c8508c775a8d916
UnextLMS <mohamed.askar@aethereus.com>
2023-03-01 20:54:49 +0530

    first commit

---
+++ api/app.js
@@ -0,0 +1,83 @@
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const helmet = require('helmet');
+const httpErrors = require('http-errors');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+const activityRouter = require('./routes/activity');
+const ngrok = require('ngrok');
+
+//var cons = require('consolidate');
+
+// view engine setup
+
+
+const app = express();
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        'default-src': ["'self'"],
+        'frame-ancestors': ["'self'", `https://mc.${process.env.STACK}.exacttarget.com`, `https://jbinteractions.${process.env.STACK}.marketingcloudapps.com`],
+      },
+    },
+  }),
+);
+
+// view engine setup
+//app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.raw({
+  type: 'application/jwt',
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// serve config
+app.use('/config.json', routes.config);
+
+// custom activity routes
+app.use('/journey/execute/', activityRouter.execute);
+app.use('/journey/save/', activityRouter.save);
+app.use('/journey/publish/', activityRouter.publish);
+app.use('/journey/validate/', activityRouter.validate);
+
+// serve UI
+app.use('/', routes.ui);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(httpErrors(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+ngrok.connect({
+  proto : 'http',
+  addr : '3000',
+}, (err, url) => {
+  if (err) {
+      console.error('Error while connecting Ngrok',err);
+      return new Error('Ngrok Failed');
+  }
+});
+
+module.exports = app;
